92511058eb029793834ff5393e1f404ce13b8c34
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2022-03-25 17:25:55 -0700

    adds formik to login and error handling for password and email

--- app/components/LoginForm.js
+++ app/components/LoginForm.js
@@ -3,41 +3,115 @@ import {
   Text,
   View,
   TextInput,
-  Button,
   Pressable,
+  TouchableOpacity,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+
+import { Formik } from "formik";
+import * as Yup from "yup";
+import Validator from "email-validator";
 
 const LoginForm = () => {
+  const LoginFormSchema = Yup.object().shape({
+    email: Yup.string().email().required("An email is required"),
+    password: Yup.string()
+      .required()
+      .min(6, "Password must have at least 8 characters"),
+  });
   return (
     <View style={styles.container}>
-      <View style={styles.inputField}>
-        <TextInput
-          placeholderTextColor="#444"
-          placeholder="Phone number, email or usename"
-          autoCapitalize="none"
-          keyboardType="email-address"
-          textContentType="emailAddress"
-          autoFocus={true}
-        />
-      </View>
-      <View>
-        <TextInput
-          style={styles.inputField}
-          placeholderTextColor="#444"
-          placeholder="Password"
-          autoCapitalize="none"
-          autoCorrect={false}
-          secureTextEntry={true}
-          textContentType="password"
-        />
-      </View>
-      <View style={styles.forgotPassword}>
-        <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
-      </View>
-      <Pressable titleSize={20} style={styles.button}>
-        <Text>Log in</Text>
-      </Pressable>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+        validationSchema={LoginFormSchema}
+        validateOnMount={true}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          isValid,
+          errors,
+        }) => (
+          <>
+            <View
+              style={[
+                styles.inputField,
+                {
+                  borderColor:
+                    values.email.length < 1 || Validator.validate(values.email)
+                      ? "#ccc"
+                      : "red",
+                },
+              ]}
+            >
+              <TextInput
+                placeholderTextColor="#444"
+                placeholder="Phone number, email or usename"
+                autoCapitalize="none"
+                keyboardType="email-address"
+                textContentType="emailAddress"
+                autoFocus={true}
+                onChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                value={values.email}
+              />
+            </View>
+            {errors.email && (
+              <Text style={{ fontSize: 10, marginBottom: 4, color: "red" }}>
+                {errors.email}
+              </Text>
+            )}
+            <View>
+              <TextInput
+                style={[
+                  styles.inputField,
+                  {
+                    borderColor:
+                      1 > values.password.length || values.password.length > 6
+                        ? "#ccc"
+                        : "red",
+                  },
+                ]}
+                placeholderTextColor="#444"
+                placeholder="Password"
+                autoCapitalize="none"
+                autoCorrect={false}
+                secureTextEntry={true}
+                textContentType="password"
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+              />
+            </View>
+            {errors.password && (
+              <Text style={{ fontSize: 10, marginBottom: 4, color: "red" }}>
+                {errors.password}
+              </Text>
+            )}
+            <View style={styles.forgotPassword}>
+              <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+            </View>
+            <Pressable
+              titleSize={20}
+              style={styles.button(isValid)}
+              onPress={handleSubmit}
+            >
+              <Text style={styles.buttonText}>Log in</Text>
+            </Pressable>
+            <View style={styles.signupContainer}>
+              <Text>Don't have an account? </Text>
+              <TouchableOpacity>
+                <Text style={styles.signUpText}>Sign Up!</Text>
+              </TouchableOpacity>
+            </View>
+          </>
+        )}
+      </Formik>
     </View>
   );
 };
@@ -45,13 +119,13 @@ const LoginForm = () => {
 export default LoginForm;
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: "yellow",
+  button: (isValid) => ({
+    backgroundColor: isValid ? "yellow" : "blue",
     alignItems: "center",
     borderRadius: 8,
     justifyContent: "center",
     minHeight: 42,
-  },
+  }),
   container: {
     marginBottom: 80,
     backgroundColor: "white",
@@ -71,5 +145,19 @@ const styles = StyleSheet.create({
   },
   forgotPasswordText: {
     color: "blue",
+  },
+  buttonText: {
+    fontWeight: "600",
+    color: "black",
+    fontSize: 20,
+  },
+  signupContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "center",
+    marginTop: 50,
+  },
+  signUpText: {
+    color: "black",
   },
 });

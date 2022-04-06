2b75835e11847c43a82cdffc03c2182fc4335061
Aly Baez <alybaez@Alys-MBP.lan>
2022-04-06 00:25:43 -0700

    adds mock up styling for chat rooms

--- app/components/chats/ChatFeed.js
+++ app/components/chats/ChatFeed.js
@@ -1,14 +1,70 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import React from "react";
+import Room from "./Room";
+export const USERS = [
+  {
+    name: "aly baez",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR39d2RW6dWiV3zzeBHJMYeYM6cn8zrB-YeAW8LuSLzHw&s",
+  },
+  {
+    name: "sammmmmmmmmm baez",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR39d2RW6dWiV3zzeBHJMYeYM6cn8zrB-YeAW8LuSLzHw&s",
+  },
+  {
+    name: "aly baez",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR39d2RW6dWiV3zzeBHJMYeYM6cn8zrB-YeAW8LuSLzHw&s",
+  },
+  {
+    name: "sammmmmmmmmm baez",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR39d2RW6dWiV3zzeBHJMYeYM6cn8zrB-YeAW8LuSLzHw&s",
+  },
+  {
+    name: "aly baez",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR39d2RW6dWiV3zzeBHJMYeYM6cn8zrB-YeAW8LuSLzHw&s",
+  },
+  {
+    name: "sammmmmmmmmm baez",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR39d2RW6dWiV3zzeBHJMYeYM6cn8zrB-YeAW8LuSLzHw&s",
+  },
+  {
+    name: "aly baez",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR39d2RW6dWiV3zzeBHJMYeYM6cn8zrB-YeAW8LuSLzHw&s",
+  },
+  {
+    name: "sammmmmmmmmm baez",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR39d2RW6dWiV3zzeBHJMYeYM6cn8zrB-YeAW8LuSLzHw&s",
+  },
+];
 
 const ChatFeed = () => {
   return (
-    <View>
-      <Text style={{ color: "white" }}>chatFeed</Text>
+    <View style={styles.container}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {USERS.map((room, index) => (
+          <View key={index} style={styles.room}>
+            <Room room={room} />
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 };
 
 export default ChatFeed;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 13,
+  },
+  room: {
+    marginHorizontal: 6,
+  },
+});
---
+++ app/components/chats/Room.js
@@ -0,0 +1,41 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
+import React from "react";
+
+const Room = ({ room }) => {
+  return (
+    <TouchableWithoutFeedback>
+      <View style={styles.container}>
+        <Image style={styles.image} source={{ uri: room.image }} />
+        <Text style={styles.name}>
+          {room.name.length > 11
+            ? room.name.slice(0, 7).toLowerCase() + "..."
+            : room.name.toLowerCase()}
+        </Text>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default Room;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    borderWidth: 3,
+    borderColor: "#ff8501",
+  },
+  name: {
+    color: "white",
+  },
+});
